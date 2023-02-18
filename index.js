4f30b12b492e29ea8115e5acfe59d52b179f2cbc
aji <wiji.permana@gmail.com>
2023-02-18 07:16:32 +0700

    change index code structure

--- index.js
+++ index.js
@@ -2,31 +2,47 @@ const express = require("express");
 const mongoose = require("mongoose");
 const sapRouter = require("./routes/sap");
 
-const PORT = 3000;
-
+// MONGOOSE SETUP
 mongoose.set("strictQuery", false);
 
+// EXPRESS SETUP
+const PORT = 3000;
 const app = express();
 app.use(express.json());
-main();
 
-async function main() {
+// INITIALIZE DB CONNECTION
+connectDB();
+
+// INITIALIZE APP
+initExpress();
+
+async function initExpress() {
 	try {
-		const mgs = await mongoose.connect("mongodb://127.0.0.1:27017/scale");
-
-		app.get("/", (req, res) => {
-			res.send("Hello World!");
-		});
+		app.get("/", helloWorld);
 
 		app.use("/sap", sapRouter);
 
 		app.listen(PORT, appListen);
-	} catch (error) {
-		console.error(error);
+	} catch (e) {
+		console.error(e);
+		process.exit(1);
+	}
+}
+
+async function connectDB() {
+	try {
+		const db = await mongoose.connect("mongodb://127.0.0.1:27017/scale");
+	} catch (e) {
+		consle.error(e);
+		console.error(e);
 		process.exit(1);
 	}
 }
 
 function appListen() {
 	console.log(`Example app listening on port ${PORT}`);
+}
+
+function helloWorld(req, res) {
+	res.send("Hello World!");
 }
