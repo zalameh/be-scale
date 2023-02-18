d91dbb4bfa99471bea2907a9aabf05b97248dba0
aji <wiji.permana@gmail.com>
2023-02-18 08:12:15 +0700

    create delete sap

--- routes/sap.js
+++ routes/sap.js
@@ -3,24 +3,38 @@ const router = express.Router();
 
 const SAP = require("../models/sap");
 
-router.post("/", (req, res) => {
+router.post("/", createSAP);
+router.delete("/", deleteAllSAP);
+
+async function createSAP(req, res) {
   const sap = new SAP({ ...req?.body });
 
-  sap
-    .save()
-    .then(result => {
-      console.log(result);
-      res.status(201).json({
-        message: "Handling POST requests to /products",
-        createdProduct: result,
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
+  try {
+    const result = await sap.save();
+    res.status(201).json({
+      message: "Handling POST requests to /products",
+      createdProduct: result,
     });
-});
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      error: e,
+    });
+  }
+}
+
+async function deleteAllSAP(req, res) {
+  try {
+    const result = await SAP.deleteMany();
+    res.status(200).json({
+      message: "Delete All",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      error: e,
+    });
+  }
+}
 
 module.exports = router;
