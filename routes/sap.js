22f1c41bbe488a864ce35db1b6401cf35708538a
aji <wiji.permana@gmail.com>
2023-02-18 08:29:26 +0700

    change routes/sap code structure

--- routes/sap.js
+++ routes/sap.js
@@ -8,33 +8,38 @@ router.delete("/", deleteAllSAP);
 
 async function createSAP(req, res) {
   const sap = new SAP({ ...req?.body });
+  let result;
 
   try {
-    const result = await sap.save();
-    res.status(201).json({
-      message: "Handling POST requests to /products",
-      createdProduct: result,
-    });
+    result = await sap.save();
   } catch (e) {
     console.log(e);
     res.status(500).json({
       error: e,
     });
   }
+
+  res.status(201).json({
+    message: "Handling POST requests to /sap",
+    sap: result,
+  });
 }
 
 async function deleteAllSAP(req, res) {
+  let result;
+
   try {
-    const result = await SAP.deleteMany();
-    res.status(200).json({
-      message: "Delete All",
-    });
+    result = await SAP.deleteMany();
   } catch (e) {
     console.log(e);
     res.status(500).json({
       error: e,
     });
   }
+
+  res.status(200).json({
+    message: "Delete All",
+  });
 }
 
 module.exports = router;
